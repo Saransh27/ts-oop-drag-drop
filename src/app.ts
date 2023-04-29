41b0292c76f066d7f3252c7df3058e5dd3d74e43
Saransh27 <mehra.saransh27@gmail.com>
2023-04-29 13:52:49 +0530

    --

--- src/app.ts
+++ src/app.ts
@@ -115,6 +115,31 @@ function autobind(_: any, _1: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+//Component Base Class
+
+class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) this.element.id = newElementId;
+  }
+}
+
 //Project List
 
 class ProjectList {
